538996ad29bff0d7932d0d3fd48fd78264bb7d38
Crowdbotics <team@crowdbotics.com>
2024-03-20 18:07:22 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "marie-cooking-47164",
+  url: "https://marie-cooking-47164.botics.co",
+  api: "https://marie-cooking-47164.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
